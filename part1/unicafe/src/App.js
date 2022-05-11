ec19f55cb8a33a54a97846e906a585d8a4c97a30
rickzen <rickzen9x@gmail.com>
2022-05-11 23:00:29 +0300

    ex1.6 fixed and 1.7 completed

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,10 +1,39 @@
 import { useState } from 'react'
 
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const Display = props => <div>{props.text} {props.value}{props.p}</div>
+
 const Statistics = (props) => {
-  return (<div>statistics {props.value}%</div>)
+  const good = props.clicks[0]
+  const neutral = props.clicks[1]
+  const bad = props.clicks[2]
+  if (good+neutral+bad>0){
+    return (
+      <div>
+        <h2>Statistics</h2>
+        <Display text="good" value={good} />
+        <Display text="neutral" value={neutral} />
+        <Display text="bad" value={bad} />
+        <Display text="all" value={good+neutral+bad} />
+        <Display text="average" value={(good-bad)/(good+neutral+bad)} />
+        <Display text="positive" value={(good)/((good+neutral+bad)/100)} p="%" />
+      </div>
+    )
+  }
+  return (
+    <div>
+      <h2>Statistics</h2>
+      No feedback given
+    </div>
+  )
 }
 
-const Display = props => <div>{props.text} {props.value}</div>
+
 
 const App = () => {
   // save clicks of each button to its own state
@@ -31,23 +60,13 @@ const App = () => {
       <Button handleClick={handleGood} text='good' />
       <Button handleClick={handleNeutral} text='neutral' />
       <Button handleClick={handleBad} text='bad' />
-      <h2>Statistics</h2>
-      <Display text="good" value={good} />
-      <Display text="neutral" value={neutral} />
-      <Display text="bad" value={bad} />
-      <Display text="all" value={good+neutral+bad} />
-      <Display text="average" value={(good-bad)/(good+neutral+bad)} />
-      <Statistics value={(good)/((good+neutral+bad)/100)} />
+      <Statistics clicks={[good,neutral,bad]} />
     </div>
     
   )
 }
 
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
+
 
 
 export default App
