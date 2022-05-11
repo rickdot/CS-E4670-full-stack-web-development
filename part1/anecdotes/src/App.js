20561fbd7817c3c04d9c17984c3a959efbe2e894
rickzen <rickzen9x@gmail.com>
2022-05-12 00:48:05 +0300

    ex1.13 completed

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,11 +1,28 @@
 import { useState } from 'react'
 
+var points = new Uint8Array(7)  // initialize an array
+
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>
     {text}
   </button>
 )
 
+const Display = ({index, anecdotes}) => {
+  return (
+    <div>
+      {anecdotes[index]}
+    </div>
+  )
+}
+
+const DisplayVote = ({index, votes}) => {
+  return (
+    <div>
+      has {votes[index]} votes
+    </div>
+  )
+}
 
 const App = () => {
   const anecdotes = [
@@ -17,17 +34,27 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
   ]
-   
+
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(points)
 
   const handleClick = () => {
     var number = Math.floor(Math.random() * 7)
     setSelected(number)
   }
 
+  const handleVote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+  }
+
+  // console.log(votes)
   return (
     <div>
-      <p> {anecdotes[selected]} </p>
+      <Display index={selected} anecdotes={anecdotes} />
+      <DisplayVote index={selected} votes={votes}/>
+      <Button handleClick={handleVote} text="vote"></Button>
       <Button handleClick={handleClick} text='next anecdote' />
     </div>
   )
