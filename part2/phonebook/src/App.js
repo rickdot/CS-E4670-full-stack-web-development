065c6512348f177bbc1b668a9ef0a0dc1ae273b5
rickzen <rickzen9x@gmail.com>
2022-05-13 00:33:35 +0300

    ex2.6 completed

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+const Person = ({person}) => 
+  <div>
+    {person.name}
+  </div>
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const newPerson = {
+      name: newName
+    }
+    setPersons(persons.concat(newPerson))
+  }
+
+  const handleNameChange = (event) => {
+    // console.log(event.target.value);
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+        {persons.map((person, i) => <Person key={i} person={person} />)}
+    </div>
+  )
+}
+
+export default App
