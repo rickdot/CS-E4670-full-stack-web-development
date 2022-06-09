6fea63d6099b3ac0f0ae6893f28b4d7222b122ab
yh <rickzen9x@gmail.com>
2022-06-09 22:10:50 +0300

    part2 1-8 finished

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,25 +2,31 @@ import { useState } from 'react'
 
 const Person = ({person}) => 
   <div>
-    {person.name}
+    {person.name} {person.number}
   </div>
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
+  // state of input fields
   const [newName, setNewName] = useState('')
+  const [newNum, setNewNum] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
-    
     const newPerson = {
-      name: newName
+      name: newName,
+      number: newNum
     }
-    // check existence
+    // check name existence
     let flag = 0
     for(let i=0; i<persons.length; i++){
-      if(JSON.stringify(persons[i]) === JSON.stringify(newPerson)){
+      if(JSON.stringify(persons[i].name) === JSON.stringify(newPerson.name)){
         flag = 1
         break
       }
@@ -31,7 +37,6 @@ const App = () => {
     } else {
       setPersons(persons.concat(newPerson))
     }
-
   }
 
   const handleNameChange = (event) => {
@@ -39,12 +44,21 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleNumChange = (event) => {
+    // console.log(event.target.value);
+    setNewNum(event.target.value)
+  }
+
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNum} onChange={handleNumChange} />
         </div>
         <div>
           <button type="submit">add</button>
