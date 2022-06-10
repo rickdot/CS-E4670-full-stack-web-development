5351975a8f73d8d6d37f24e8093ae3f69ee2fb58
yh <rickzen9x@gmail.com>
2022-06-10 05:01:45 +0300

    part2 1-11 finished

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 
 
 const Filter = (props) => 
@@ -33,17 +35,26 @@ const Persons = (props) =>
   
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
 
   // state of input fields
   const [newName, setNewName] = useState('')
   const [newNum, setNewNum] = useState('')
   const [filterVal, setFilterVal] = useState('')
+
+
+  const hook = () => {
+    // console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        // console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }
+  
+  useEffect(hook, [])
+
 
   const addPerson = (event) => {
     event.preventDefault()
