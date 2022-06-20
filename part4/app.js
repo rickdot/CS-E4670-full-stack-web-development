b4a719065eaff8ddf695dcf9ea5490ca0d3bad52
yh <rickzen9x@gmail.com>
2022-06-21 00:35:49 +0300

    part4 1-15

--- part4/app.js
+++ part4/app.js
@@ -2,10 +2,15 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose')
+
+
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
-const mongoose = require('mongoose')
+
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -23,6 +28,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
